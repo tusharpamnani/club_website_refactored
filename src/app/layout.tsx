95af37519b7216c53c1b73fe3c8c0f4f8a79e774
Khanba22 <khanba2222khan@gmail.com.com>
2025-02-02 20:13:39 +0530

    refactored images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import NavBar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,7 +19,6 @@ export default function RootLayout({
       >
         <NavBar />
         {children}
-        <Footer />
       </body>
     </html>
   );
